5829695b1c3283d91740c67aa2366f7dc822f6b9
Karry Mao <karrym@andrew.cmu.edu>
2024-09-05 21:04:55 -0400

    Add more test cases

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
-
         Renderer renderer = new Renderer(rectangle);
+        renderer.draw();
 
+        Square square = new Square(4);
+        renderer = new Renderer(square);
+        renderer.draw();
+
+        Circle circle = new Circle(5);
+        renderer = new Renderer(circle);
         renderer.draw();
     }
 }
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,16 @@
 import { Renderer } from "./renderer.js"
 import { Rectangle } from "./shapes/rectangle.js";
+import { Square } from "./shapes/square.js";
+import { Circle } from "./shapes/circle.js";
 
 const rectangle = new Rectangle(10, 5);
 const renderer = new Renderer(rectangle)
 renderer.draw();
+
+const square = new Square(10);
+const renderer2 = new Renderer(square)
+renderer2.draw();
+
+const circle = new Circle(10);
+const renderer3 = new Renderer(circle)
+renderer3.draw();
